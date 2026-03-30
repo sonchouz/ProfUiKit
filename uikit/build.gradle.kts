62bdb45bf0cb9ae13ca07fea899046330fcb83c8
sonchouz <sofianikonorova41@gmail.com>
2026-03-30 11:44:48 +0700

    lib publication

--- uikit/build.gradle.kts
+++ uikit/build.gradle.kts
@@ -1,14 +1,14 @@
+import org.gradle.api.publish.maven.MavenPublication
+
 plugins {
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.kotlin.compose)
+    id("com.android.library")
+    id("org.jetbrains.kotlin.plugin.compose")
     id("maven-publish")
 }
 
 android {
     namespace = "com.example.uikit"
-    compileSdk {
-        version = release(36)
-    }
+    compileSdk = 36
 
     defaultConfig {
         minSdk = 34
@@ -26,12 +26,21 @@ android {
             )
         }
     }
+
     buildFeatures {
         compose = true
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
+    }
+    
+
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+        }
     }
 }
 
@@ -39,15 +48,14 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    implementation(libs.androidx.compose.runtime.annotation)
+
     implementation("androidx.compose.runtime:runtime")
     implementation("androidx.compose.ui:ui")
     implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.compose.runtime)
     implementation(libs.androidx.material3)
-    implementation(libs.compose.material3)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.compose.foundation)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -60,7 +68,7 @@ afterEvaluate {
                 from(components["release"])
 
                 groupId = "com.github.sonchouz"
-                artifactId = "ComposeUiKit"
+                artifactId = "ProfUiKit"
                 version = "1.0.4"
             }
         }
